96a9e9e3df7ce90a4ae8b6002e3213204b52a4bd
ryotaro23 <ryo2017taro@icloud.com>
2022-05-26 02:04:19 +0900

    feat:materialuiをリストに実装

--- one-day-youth-bootcamp-frontend/node_modules/react-typescript/src/components/TaskList.tsx
+++ one-day-youth-bootcamp-frontend/node_modules/react-typescript/src/components/TaskList.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
-import { Task } from '../'
-// import {Deadline} from '../'
+import { Task } from '../';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
+
+const useStyles = makeStyles((theme:any) => ({
+  title: {
+    color: 'red',
+    fontSize:100,
+    fontWeight:'bold',
+  },
+  subTitle:{
+    fontSize:20,
+  },
+}));
 
 type Props = {
   tasks: Task[];
@@ -21,20 +40,35 @@ export const TaskList: React.FC<Props> = ({ tasks, setTasks,deadline ,setDeadlin
     setTasks(newTasks);
   };
 
+  const classes = useStyles();
+
   return (
     <ul>
       {tasks.map((task, index) => (
-        <li key={`todo-${index}`}>
-          {task.isDone ? <s>{task.label}{task.oneday}</s> : <i> {task.label}{task.oneday}</i>}
-
-          <input
-            onChange={(e) => handleCheckBox(e, index)}
-            type="checkbox"
-            checked={task.isDone}
-          /> 
-        </li>
-      ))}
-     
+        <>
+        <List>
+          <ListItem key={`todo-${index}`} role={undefined} dense divider >
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                checked={task.isDone}
+                tabIndex={-1}
+                disableRipple
+                onChange={(e) => handleCheckBox(e, index)}
+              />
+            </ListItemIcon>
+            <ListItemText className={classes.title} primary={
+              task.isDone ? <s>{task.label}{task.oneday}</s> : <i> {task.label}{task.oneday}</i>}/>
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <CommentIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+    </List>
+            </>
+      ))}     
     </ul>
+    
   );
 };
